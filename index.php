5c1524b8b0154c685fc233ac220e67e55a31858a
luirusso <luirusso_@hotmail.it>
2021-12-21 16:21:05 +0100

    completed php version

--- index.php
+++ index.php
@@ -13,26 +13,30 @@ require_once __DIR__ . '/data/database.php';
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- CSS -->
-    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Document</title>
 </head>
 <body>
-    <header>
-
+    <header class="d-flex align-items-center">
+        <div class="container">
+            <a href="./"><img class="logo" src="./img/logo.png" alt=""></a>
+        </div>
     </header>
 
     <main>
-        <div class="container">
-            <div class="row">
+        <div class="container small">
+            <div class="row justify-content-center">
                 <?php foreach($database as $data) : ?>
-                <div class="col-4">
-                    <div>
-                        <img src=" <?php echo $data['poster']; ?>" alt="<?php echo $data['title']; ?>">
+                <div class="col-3 card mx-4 my-5 py-4">
+                    <div class="text-center">
+                        <div class="pb-3">
+                            <img src=" <?php echo $data['poster']; ?>" alt="<?php echo $data['title']; ?>">
+                        </div>
+                        <div><?php echo $data['title']; ?></div>
+                        <div><?php echo $data['author']; ?></div>
+                        <div><?php echo $data['year']; ?></div>
+                        <div><?php echo $data['genre']; ?></div>
                     </div>
-                    <div><?php echo $data['title']; ?></div>
-                    <div><?php echo $data['author']; ?></div>
-                    <div><?php echo $data['year']; ?></div>
-                    <div><?php echo $data['genre']; ?></div>
                 </div>
                 <?php endforeach; ?>
             </div>
