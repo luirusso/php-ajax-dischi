dba46147ef2637292adf51d2b9b9c90d73f4a058
luirusso <luirusso_@hotmail.it>
2021-12-21 16:28:52 +0100

    completed php version

--- php-version/data/database.php
+++ php-version/data/database.php
@@ -44,8 +44,4 @@ $database = [
         'genre' => 'Rock'
     ]
 ];
-
-// header('Content-Type: application/json');
-
-// echo json_encode($database);
 ?>
--- php-version/index.php
+++ php-version/index.php
@@ -1,7 +1,7 @@
 <!-- Import data -->
 <?php
 
-require_once __DIR__ . '/php-version/data/database.php';
+require_once __DIR__ . '/data/database.php';
 ?>
 
 <!DOCTYPE html>
@@ -13,13 +13,13 @@ require_once __DIR__ . '/php-version/data/database.php';
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- CSS -->
-    <link rel="stylesheet" href="./php-version/style/style.css">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Document</title>
 </head>
 <body>
     <header class="d-flex align-items-center">
         <div class="container">
-            <a href="./"><img class="logo" src="./php-version/img/logo.png" alt=""></a>
+            <a href="./"><img class="logo" src="./img/logo.png" alt=""></a>
         </div>
     </header>
 
@@ -29,10 +29,10 @@ require_once __DIR__ . '/php-version/data/database.php';
                 <?php foreach($database as $data) : ?>
                 <div class="col-3 card mx-4 my-5 py-4">
                     <div class="text-center">
-                        <div class="pb-4">
-                            <a href="#"><img src="<?php echo $data['poster']; ?>" alt="<?php echo $data['title']; ?>"></a>
+                        <div class="pb-3">
+                            <img src=" <?php echo $data['poster']; ?>" alt="<?php echo $data['title']; ?>">
                         </div>
-                        <h5><?php echo $data['title']; ?></h5>
+                        <div><?php echo $data['title']; ?></div>
                         <div><?php echo $data['author']; ?></div>
                         <div><?php echo $data['year']; ?></div>
                         <div><?php echo $data['genre']; ?></div>
